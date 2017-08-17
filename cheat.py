97eef1f159a6f884d3b7ca24ee70790b80409caa
Geovani <geovanij3@gmail.com>
2017-08-17 09:23:07 -0300

    created game class

--- cheat.py
+++ cheat.py
@@ -3,6 +3,11 @@ import random
 
 
 class Card(object):
+	'''
+	Card class.
+
+	Used to store the rank and suit of a card.
+	'''
 
 	SUITS_LEN = 4
 	SUITS = {
@@ -30,34 +35,88 @@ class Card(object):
 	}
 
 	def __init__(self, rank, suit):
+		'''
+		Constructor.
+
+		Args:
+			rank: Rank of the card.
+			suit: Suit of card.
+		'''
+
 		super(Card, self).__init__()
 		self.rank = rank
 		self.suit = suit
 
-	def __str__(self):
-		return "[%s, %s]" % (self.rank, self.suit)	
 
-class Deck(object):
+	def __str__(self):
+		return "%s%s" % (self.rank, self.suit)
+
+
+	def __repr__(self):
+		return self.__str__()
+
+
+
+class Deck(list):
 
 	def __init__(self):
 		super(Deck, self).__init__()
-		self.cards = []
+		self._generate()
 
-	def generate(self):
-		self.cards.clear()
+	def _generate(self):
+		self.clear()
 		for s in Card.SUITS:
 			suit = Card.SUITS[s]
 			for r in Card.RANKS:
 				rank = Card.RANKS[r]
-				self.cards.append(Card(rank, suit))
+				self.append(Card(rank, suit))
 
 	def shuffle(self):
-		random.shuffle(self.cards)
+		random.shuffle(self)
+		return self
 
 
 
-d = Deck()
-d.generate()
-d.shuffle()
-for i in d.cards:
-	print(i)
+class Player(list):
+
+	def __init__(self):
+		super(Player, self).__init__()
+
+
+
+class Game(object):
+
+	START_CARDS_LEN = 13
+	START_PLAYERS_LEN = 4
+
+	def __init__(self):
+		super(Game, self).__init__()
+		self.deck = Deck().shuffle()
+
+		self.players = [ Player() for i in range(self.START_PLAYERS_LEN) ]
+
+		for i in range(len(self.players)):
+			c = [ self.deck.pop() for j in range(self.START_CARDS_LEN) ]
+			self.players[i].extend(c)
+
+
+
+	def play(self):
+
+		for p in circ(players):
+			# p joga
+			# outros player acusam, ou n, p
+			# check(mao vazia)
+			# primo p
+
+
+
+
+
+
+
+
+
+
+
+
